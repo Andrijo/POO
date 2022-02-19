8f7ada53c7c3d09b64fb05f621e1c71ace521851
andrijo <andyespino07@hotmail.com>
2022-02-18 17:08:35 -0700

    Se agregan las prácticas

---
+++ src/Practicas/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,15 @@
+package Practicas;
+
+/**
+ * PALABRAS EN AZUL: palabras reservadas: uso exclusivo del lenguaje PALABRAS EN
+ * NEGRO: IDENTIFICADORES --> variables, constantes, clases, funciones, objetos,
+ * etc []: arreglo {}: delimitadores --> bloques de código
+ *
+ * @author Andy Espino Cabrales
+ */
+public class EVA1_1_HOLAMUNDO { //Cuerpo de la clase
+
+    public static void main(String[] args) { //Función
+        System.out.println("Hello world!");
+    }
+}
