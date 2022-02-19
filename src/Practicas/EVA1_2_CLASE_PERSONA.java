a31f3a1b8845a55a93df4276dab5b4b6a5050a5b
andrijo <andyespino07@hotmail.com>
2022-02-18 17:24:48 -0700

    Momento

---
+++ src/Practicas/EVA1_2_CLASE_PERSONA.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practicas;
+
+/**
+ *
+ * @author User
+ */
+public class EVA1_2_CLASE_PERSONA {
+    
+}
