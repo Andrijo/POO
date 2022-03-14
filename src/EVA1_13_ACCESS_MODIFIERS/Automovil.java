2e157ce1eb2e8aed3214cbddc4cd64ccdae65a8a
Andy Espino <65322580+Andrijo@users.noreply.github.com>
2022-03-14 11:35:26 -0700

    Add files via upload

---
+++ src/EVA1_13_ACCESS_MODIFIERS/Automovil.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EVA1_13_ACCESS_MODIFIERS;
+
+/**
+ *
+ * @author User
+ */
+public class Automovil {
+
+    private String modelo;
+    private String marca;
+    private int año;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+    
+    void imprimirMensaje(){ //Metodo con modificador default
+        System.out.println("Hola mundo");
+    }
+
+}
+
+class Carreta { //Modificador de acceso --> default
+
+    private String marca;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+}
