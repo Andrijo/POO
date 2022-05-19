a9d08d4e120e8d5e99f5687d9685265d19783da8
Andy Espino <65322580+Andrijo@users.noreply.github.com>
2022-05-18 19:16:32 -0600

    Update EVA2_6_HERENCIA_INTERFACES.java

--- src/EVA2_6_HERENCIA_INTERFACES/EVA2_6_HERENCIA_INTERFACES.java
+++ src/EVA2_6_HERENCIA_INTERFACES/EVA2_6_HERENCIA_INTERFACES.java
@@ -7,8 +7,82 @@ package EVA2_6_HERENCIA_INTERFACES;
 
 /**
  *
- * @author User
+ * @author Andrijo
  */
 public class EVA2_6_HERENCIA_INTERFACES {
-    
+
+    public static void main(String[] args) {
+        Estudiante student = new Estudiante("LC21447", "Andy", 19);
+        student.imprimirDatos();
+    }
 }
+
+class Estudiante extends Persona implements MuestraDatos {
+
+    private String numeroControl;
+
+    public Estudiante() {
+        super(); //Constructor de persona
+        this.numeroControl = "";
+    }
+
+    public Estudiante(String numeroControl, String nombre, int edad) {
+        super(nombre, edad); //Constructor de persona
+        this.numeroControl = numeroControl;
+    }
+
+    public String getNumeroControl() {
+        return numeroControl;
+    }
+
+    public void setNumeroControl(String numeroControl) {
+        this.numeroControl = numeroControl;
+    }
+
+    @Override
+    public void imprimirDatos() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Edad: " + getEdad());
+        System.out.println("Número de control: " + getNumeroControl());
+    }
+
+}
+
+interface MuestraDatos {
+
+    public void imprimirDatos();
+}
+
+class Persona {
+
+    private String nombre;
+    private int edad;
+
+    public Persona() {
+        this.nombre = "";
+        this.edad = 0;
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+}
+
