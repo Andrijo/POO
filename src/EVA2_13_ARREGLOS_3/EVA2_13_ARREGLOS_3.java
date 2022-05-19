c6a80aea72a5bc21319bd807314911fa5f0b5552
Andy Espino <65322580+Andrijo@users.noreply.github.com>
2022-05-18 19:02:53 -0600

    Create EVA2_13_ARREGLOS_3.java

---
+++ src/EVA2_13_ARREGLOS_3/EVA2_13_ARREGLOS_3.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EVA2_13_ARREGLOS_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andrijo
+ */
+public class EVA2_13_ARREGLOS_3 {
+
+    public static void main(String[] args) {
+        Scanner momento = new Scanner(System.in);
+        Persona[] arrayPeople = new Persona[10];
+        for (int i = 0; i < 10; i++) {
+            //Crear el objeto
+            arrayPeople[i] =  new Persona();
+            System.out.println("¿Cuál es tu nombre?: ");
+            arrayPeople[i].setNombre(momento.nextLine());
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nombre: " + arrayPeople[i].getNombre());
+        }
+    }
+}
+
+class Persona {
+
+    private String nombre;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
