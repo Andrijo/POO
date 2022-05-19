4e5854fda0fcc1f1b018c55f06804521ffb76094
Andy Espino <65322580+Andrijo@users.noreply.github.com>
2022-05-18 19:20:36 -0600

    Create EVA2_7_INTERFACES.java

---
+++ src/EVA2_7_INTERFACES/EVA2_7_INTERFACES.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EVA2_7_INTERFACES;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ *
+ * @author Andrijo
+ */
+public class EVA2_7_INTERFACES {
+
+    public static void main(String[] args) {
+        //ARRAYS, LISTAS, ARRAYLIST, STACK (PILAS), QUEUES (COLAS)
+        ArrayList myArrayList = new ArrayList();
+        LinkedList myLinkedList = new LinkedList();
+        Stack myStack = new Stack();
+    }
+}
