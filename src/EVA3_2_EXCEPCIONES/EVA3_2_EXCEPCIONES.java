a3f40d77008bd1ea65922f3f44d7d55cef7cf569
Andy Espino <65322580+Andrijo@users.noreply.github.com>
2022-05-24 18:35:32 -0600

    Create EVA3_2_EXCEPCIONES.java

---
+++ src/EVA3_2_EXCEPCIONES/EVA3_2_EXCEPCIONES.java
@@ -0,0 +1,18 @@
+package EVA3_2_EXCEPCIONES;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andrijo
+ */
+public class EVA3_2_EXCEPCIONES {
+
+    public static void main(String[] args) {
+        Scanner momento = new Scanner(System.in);
+        int edad;
+        System.out.println("Introduce tu edad (entero): ");
+        edad = momento.nextInt();
+        System.out.println("Edad: " + edad);
+    }
+}
